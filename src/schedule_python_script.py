901be0f9c9e1e1a82219ba46ca7468611154903b
Chika-Jinanwa <46027071+Chika-Jinanwa@users.noreply.github.com>
2021-09-03 22:07:04 -0400

    Updated cron job scheduler to use customized configurations

--- src/schedule_python_script.py
+++ src/schedule_python_script.py
@@ -1,12 +1,12 @@
-from accessories import CRON_TAB, VIRTUAL_ENV
+from accessories import CRON_TAB, VIRTUAL_ENV, FREQUENCY_IN_MINUTES, REPO_NAME
 from pathlib import Path
 from crontab import CronTab
 
 dir_path = str(Path().absolute())  #Path of current working directory 
 command = 'source ' + dir_path + '/.env' + ' && ' + 'cd ' + dir_path + ' && '+ VIRTUAL_ENV + ' add_users.py'
 with CronTab(user=True) as cron:
-    job = cron.new(command=command, comment = "Cron Job that automatically adds collaborators to github repo every 5 minutes")
-    job.minute.every(1)
+    job = cron.new(command=command, comment = "Cron Job That Automatically Adds Collaborators to Github Repo: " + str(REPO_NAME) + " every " + str(FREQUENCY_IN_MINUTES) + " minute(s)")
+    job.minute.every(FREQUENCY_IN_MINUTES)
     #cron.remove_all() #uncomment line to remove all cron jobs once you feel satisfied
     #remember to run this script again using source main.sh
 
